aa0f761600883bf3b5a24a1d40a3dbcd853109eb
Yogesh Negi <113903081+itsyogeshnegi@users.noreply.github.com>
2024-03-06 15:42:50 +0530

    vercel.json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/api/users");
+        const response = await axios.get("http://localhost:3000/users");
         setUsers(response.data);
         console.log(response.data);
       } catch (error) {
